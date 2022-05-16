c5477ed9879a59f9a290e23f4d98b1f7b24d5333
Muntasir Abdullah Mizan <muntasir@kinetik.care>
2022-05-16 20:26:44 +0600

    WebElements

---
+++ 2.WebElement/Login_Test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# 1. Chrome config
+driver = webdriver.Chrome(
+    executable_path="F:\Training\eSkillBd\Batch 1\Project\WebAutomationB01\Drivers\chromedriver.exe")
+
+# 2. Screen full
+driver.maximize_window()
+
+driver.get("https://demo.opencart.com/index.php?route=account/login")
+
+driver.find_element(By.ID, 'input-email').send_keys("invalidemail@test.com")
+driver.find_element(By.ID, 'input-password').send_keys("saas23142134")
+driver.find_element(By.XPATH, '//*[@id="content"]/div/div[2]/div/form/input').click()
