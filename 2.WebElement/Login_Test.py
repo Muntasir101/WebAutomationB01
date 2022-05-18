ed75f388dfb262e280f27ea5cc99f948c4ea564a
Muntasir Abdullah Mizan <muntasir@kinetik.care>
2022-05-18 20:27:10 +0600

    WebEleemnt,Scroll,Wait

--- 2.WebElement/Login_Test.py
+++ 2.WebElement/Login_Test.py
@@ -10,6 +10,16 @@ driver.maximize_window()
 
 driver.get("https://demo.opencart.com/index.php?route=account/login")
 
+# Invalid Test
+driver.find_element(By.ID, 'input-email').clear()
 driver.find_element(By.ID, 'input-email').send_keys("invalidemail@test.com")
+driver.find_element(By.ID, 'input-password').clear()
 driver.find_element(By.ID, 'input-password').send_keys("saas23142134")
+driver.find_element(By.XPATH, '//*[@id="content"]/div/div[2]/div/form/input').click()
+
+# Valid Test
+driver.find_element(By.ID, 'input-email').clear()
+driver.find_element(By.ID, 'input-email').send_keys("user6@bd.com")
+driver.find_element(By.ID, 'input-password').clear()
+driver.find_element(By.ID, 'input-password').send_keys("123456")
 driver.find_element(By.XPATH, '//*[@id="content"]/div/div[2]/div/form/input').click()
