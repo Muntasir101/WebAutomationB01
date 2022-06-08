9e42733f7c9e84c37536fef6f4b556ea8de2fb7d
Muntasir Abdullah Mizan <muntasir@kinetik.care>
2022-06-08 20:28:43 +0600

    Framework done - Page object, Data Driven

--- Framework/test/login_test.py
+++ Framework/test/login_test.py
@@ -1,8 +1,9 @@
 import time
 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from login_page import LoginPage
+from pages.login_page import LoginPage
 import unittest
 
 from selenium.webdriver.chrome.service import Service
@@ -10,7 +11,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 class LoginTests(unittest.TestCase):
 
-    def test_validLogin(self):
+    def test_invalid_Login(self):
         baseURL = "https://demo.opencart.com"
        # driver = webdriver.Firefox(executable_path="F:\Training\eSkillBd\Batch 1\Tools\geckodriver-v0.31.0-win64_2\geckodriver.exe")
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -21,9 +22,22 @@ class LoginTests(unittest.TestCase):
 
         lp = LoginPage(driver)
         lp.login("test@email.com", "abcabc")
+        lp.login('test2@mail.com', '12342342')
 
-        userIcon = driver.find_element(By.XPATH, ".//*[@id='navbar']//span[text()='User Settings']")
-        if userIcon is not None:
-            print("Login Successful")
-        else:
-            print("Login Failed")
+        driver.close()
+
+    @pytest.mark.skip
+    def test_valid_Login(self):
+        baseURL = "https://demo.opencart.com"
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.maximize_window()
+        driver.implicitly_wait(3)
+        driver.get(baseURL)
+        time.sleep(3)
+
+        lp = LoginPage(driver)
+        lp.login("test@email.com", "abcabc")
+        lp.login('test2@mail.com', '12342342')
+
+        driver.close()
+
